13061c07f1e6bdd933ab2c6cac61852eb8cc6b02
Giacomo Tenti <giacomotenti@MBP-di-Giacomo.home>
2021-02-10 10:21:16 +0100

    initial commmit

---
+++ modules/update.py
@@ -0,0 +1,15 @@
+from modules.wavefun import psi
+
+def update(x,alpha,t,t2,V,L,which):
+    Lambda = V * L
+    psix = psi(x,alpha,L,which)
+    psi_l = psi(x-1,alpha,L,which)
+    psi_r = psi(x+1,alpha,L,which)
+    psi_l2 = psi(x-2,alpha,L,which)
+    psi_r2 = psi(x+2,alpha,L,which)
+    vsf = t2 * (psi_l2 + psi_r2) / psix
+    bx = -V * x - vsf + Lambda + t * (psi_l + psi_r) /psix
+    elx = -t * (psi_l + psi_r) /psix + vsf + V * x
+    P_left = t * psi_l / psix / bx
+    P_right = t * psi_r /psix / bx
+    return bx / Lambda , elx , P_left , P_right
---
+++ modules/wavefun.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def psi(x,alpha,L,which):
+    if which == 0:
+        if x<1 or x>L:
+            return 0
+        else:
+            return 1
+    if which == 1:
+        if x < 1 or x > L:
+            return 0
+        else:
+            return np.exp( - alpha * x)
+    if which == 2:
+        if x<1 or x > L:
+            return 0
+        else:
+            return np.exp(- alpha[0]*x) * x**alpha[1]
+#WAVEFUNCTION FOR HEISENBERG MODEL
+#def marsh
