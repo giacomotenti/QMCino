a99f67df47b33ed02452ac8bd9c6e2788569710c
afiorent <alfredofiorentino@hotmail.it>
2021-02-23 00:21:00 +0100

    e0 shifted

--- heisenberg/update.py
+++ heisenberg/update.py
@@ -7,7 +7,7 @@ import numpy as np
 @jit(nopython=True)
 def computetab(tab,conf,L):
     Lambda = L / 4.0
-    e0 = L* (np.log(2.0) - 0.25)
+    e0 = Lambda+L* (np.log(2.0) - 0.25)
     diag = 0
     el = 0
     for i in range(L):
@@ -21,13 +21,13 @@ def computetab(tab,conf,L):
             tab[i] =False # no move is possible
     el+=diag
     bx=Lambda-el
-    return bx/e0 , tab, el
+    return bx/e0 , tab, el,diag
 @jit(nopython=True)
-def update(conf,tab,iout,el):
+def update(conf,tab,iout,el,diag):
     L=conf.shape[0]
     Lambda=L/4.0
-    e0 = L* (np.log(2.0) - 0.25)
-    diag=0
+    e0 =Lambda+ L* (np.log(2.0) - 0.25)
+    #diag=0
     iback=np.mod(iout-1,L)
     if conf[iback]!= conf[iout]:
         tab[iback] =True
@@ -47,24 +47,24 @@ def update(conf,tab,iout,el):
         diag = diag + 0.5
         tab[jout] =False # no move is possible
         el+=1
-    el+=diag
-    bx=Lambda-el
-    return bx/e0 , tab, el
+    #el+=diag
+    #bx=Lambda-el
+    return   tab, el,diag
 
 #example
-#
-#L=4
-#tab=np.empty(L,dtype=bool)
-#
-#conf=[np.mod(i,2)==0 for i in range(L)]
-#conf=np.array(conf,dtype=bool)
-#print("stato=",conf)
-#bx,tab,el=computetab(tab,conf,L)
-#print("tab iniziale=",tab)
-#print("energia locale= ",el)
-#iout=0
-#conf[0]=np.invert(conf[0])
-#conf[1]=np.invert(conf[1])
-#
-#print("configurazione post",conf)
-#print(update(conf,tab,iout,el))
+L=4
+tab=np.empty(L,dtype=bool)
+
+conf=[np.mod(i,2)==0 for i in range(L)]
+conf=np.array(conf,dtype=bool)
+print("stato=",conf)
+bx,tab,el,diag=computetab(tab,conf,L)
+print("diag",diag)
+print("tab iniziale=",tab)
+print("energia locale= ",el)
+iout=0
+conf[0]=np.invert(conf[0])
+conf[1]=np.invert(conf[1])
+
+print("configurazione post",conf)
+print("tab, el e diag= ",update(conf,tab,iout,el,diag))
