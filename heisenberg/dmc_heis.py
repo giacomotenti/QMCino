9f4fe01affdc388a2505e36732cd27048799d3a2
enrdrigo <72918800+enrdrigo@users.noreply.github.com>
2021-02-22 19:01:12 +0100

    Update dmc_heis.py
    ho sistemato alcune cose. manca ancora la parte dell'update.

--- heisenberg/dmc_heis.py
+++ heisenberg/dmc_heis.py
@@ -7,28 +7,30 @@ import random
 def dmc_heis(conf,nit):
     #L=4
     #la=L/4.
-    #nw=1
+    #nw=2
     #diag=-1
-    #tab=np.array((True, True, True, True))
-    for i in range(nit):
+    #tab=np.zeros((nw,L))
+    #tab[0]=np.array((True, True, True, True))
+    #tab[1]=np.array((True, True, True, True))
+    for i in range(int(nit)):
         for w in range(nw):
             r=random.random()
             gn=np.zeros(L+1)
             gn[0]=la-diag
             for j in range(L):
-                if tab[j]: gn[j+1]=0.5
+                if tab[w,j]: gn[j+1]=0.5
             ztry=r*np.sum(gn)
             ntry=gn[0]
             iout=-1
             while(ntry<ztry):
                 iout+=1
                 ntry+=gn[iout+1]
-                print(iout, ntry, ztry)
                 if iout>L:
                     print('molto male')
                     break
             jout=np.mod(iout+1, L)
-            if iout!=-1:conf[iout]=not(conf[iout])
-            if iout!=-1:conf[jout]=not(conf[jout])
+            if iout!=-1:conf[w,iout]=not(conf[w,iout])
+            if iout!=-1:conf[w,jout]=not(conf[w,jout])
             #update....
-        return conf
+    return conf
+
